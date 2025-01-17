a9c24ddfd68ad884cfb34479514b28df47cf6ed5
Wesleykfg <wesleykelvin59@gmail.com>
2025-01-17 17:55:07 -0300

    Adicionando a função de digitação dos nomes e o array para os nomes

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Criando Variaveis
+var listaDeNomesSorteados = []; //também poderia ser let listaDeNomesSorteados = [];
+
+// Criando Funções
+// Função de adcionar os amigos ao array listaDeNomesSorteados
+function adicionarAmigo() {
+    var nomeAmigo = document.querySelector('input').value;
+    if(nomeAmigo == '') {
+        alert("Por favor, insira um nome.");
+        console.log(listaDeNomesSorteados);
+    } else {
+        listaDeNomesSorteados.push(nomeAmigo);
+        alert("O nome: " + nomeAmigo + " foi adicionado.");
+        console.log(listaDeNomesSorteados);
+    }
+    limparCampo();
+}
+
+// Função para Limpar o campo de imput
+function limparCampo() {
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+}
